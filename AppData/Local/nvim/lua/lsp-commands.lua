40a642ea45f7c75cfeb7be758e7f3fcaa40ae766
SIeepwalker <71443833+SIeepwalker@users.noreply.github.com>
2024-05-23 18:09:41 +0200

    Delete AppData/Local/nvim directory

--- AppData/Local/nvim/lua/lsp-commands.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.typescript = {
-    organizeImports = function ()
-        vim.lsp.buf.execute_command({command = "_typescript.organizeImports", arguments = {vim.fn.expand("%:p")}})
-    end
-}
-
-return M
